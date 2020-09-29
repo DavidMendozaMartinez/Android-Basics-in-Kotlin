f83552d96bc12f1c27c5bfd6a6b55a29c91d0c46
David Mendoza Martínez <david.mendoza.mnez@gmail.com>
2020-09-29 17:32:57 +0200

    Unit 2 - Add imageResourceId to the Affirmation class. Add images to the project. Add an ImageView to the list item layout. Update the ItemAdapter to set the image. Change the app theme colors and the dark theme colors. Change the app icon

--- unit_2/Affirmations/app/src/main/java/com/example/affirmations/adapter/ItemAdapter.kt
+++ unit_2/Affirmations/app/src/main/java/com/example/affirmations/adapter/ItemAdapter.kt
@@ -4,25 +4,27 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.affirmations.R
 import com.example.affirmations.model.Affirmation
 
 /**
- * Adapter for the [RecyclerView] in [MainActivity]. Displays [Affirmation] data object.
+ * Adapter for the [RecyclerView] in MainActivity. Displays [Affirmation] data object.
  */
 class ItemAdapter(
     private val context: Context,
-    private val dataset: List<Affirmation>
+    private val dataSet: List<Affirmation>
 ) : RecyclerView.Adapter<ItemAdapter.ItemViewHolder>() {
 
     // Provide a reference to the views for each data item
     // Complex data items may need more than one view per item, and
     // you provide access to all the views for a data item in a view holder.
     // Each data item is just an Affirmation object.
-    class ItemViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
+    class ItemViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         val textView: TextView = view.findViewById(R.id.item_title)
+        val imageView: ImageView = view.findViewById(R.id.item_image)
     }
 
     /**
@@ -40,12 +42,13 @@ class ItemAdapter(
      * Replace the contents of a view (invoked by the layout manager)
      */
     override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
-        val item = dataset[position]
+        val item = dataSet[position]
         holder.textView.text = context.resources.getString(item.stringResourceId)
+        holder.imageView.setImageResource(item.imageResourceId)
     }
 
     /**
      * Return the size of your dataset (invoked by the layout manager)
      */
-    override fun getItemCount() = dataset.size
+    override fun getItemCount() = dataSet.size
 }
