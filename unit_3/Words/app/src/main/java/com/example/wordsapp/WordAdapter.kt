e83311c2a7c70c5ad38b70aa3bdce8cee793fa66
David Mendoza <david.mendoza.mnez@gmail.com>
2021-01-21 21:33:43 +0100

    Unit 3 - Modify the Words app to use a single activity and multiple fragments, and navigate between fragments with the Navigation Component

--- unit_3/Words/app/src/main/java/com/example/wordsapp/WordAdapter.kt
+++ unit_3/Words/app/src/main/java/com/example/wordsapp/WordAdapter.kt
@@ -28,7 +28,7 @@ import androidx.annotation.RequiresApi
 import androidx.recyclerview.widget.RecyclerView
 
 /**
- * Adapter for the [RecyclerView] in [DetailActivity].
+ * Adapter for the [RecyclerView] in [WordListFragment].
  */
 class WordAdapter(private val letterId: String, context: Context) :
     RecyclerView.Adapter<WordAdapter.WordViewHolder>() {
@@ -53,7 +53,7 @@ class WordAdapter(private val letterId: String, context: Context) :
     }
 
     class WordViewHolder(val view: View) : RecyclerView.ViewHolder(view) {
-        val button = view.findViewById<Button>(R.id.button_item)
+        val button: Button = view.findViewById(R.id.button_item)
     }
 
     override fun getItemCount(): Int = filteredWords.size
@@ -84,12 +84,13 @@ class WordAdapter(private val letterId: String, context: Context) :
         // Set the text of the WordViewHolder
         holder.button.text = item
         holder.button.setOnClickListener {
-            val queryUrl: Uri = Uri.parse("${DetailActivity.SEARCH_PREFIX}$item")
+            val queryUrl: Uri = Uri.parse("${WordListFragment.SEARCH_PREFIX}$item")
             val intent = Intent(Intent.ACTION_VIEW, queryUrl)
             context.startActivity(intent)
         }
 
     }
+
     // Setup custom accessibility delegate to set the text read with
     // an accessibility service
     companion object Accessibility : View.AccessibilityDelegate() {
